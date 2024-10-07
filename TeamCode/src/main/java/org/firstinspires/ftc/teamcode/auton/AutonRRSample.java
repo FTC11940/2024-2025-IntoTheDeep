046f9d8de6fd5395199c7a4fdacbc7b9db463c8c
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2024-10-07 15:46:01 -0400

    Update AutonRRSample.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/AutonRRSample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/AutonRRSample.java
@@ -4,6 +4,7 @@ package org.firstinspires.ftc.teamcode.auton;
 import com.acmerobotics.dashboard.config.Config;
 
 // Non-RR imports
+import com.acmerobotics.roadrunner.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
@@ -12,13 +13,15 @@ import org.firstinspires.ftc.teamcode.MecanumDrive;
 @Config
 @Autonomous(name = "Auton RR Sample", group = "Autonomous")
 
-public abstract class AutonRRSample extends LinearOpMode {
+public class AutonRRSample extends LinearOpMode {
 
     private MecanumDrive drive;
 
+    private Pose2d pose = new Pose2d(0, 0, 0);
+
     @Override
     public void runOpMode() {
-        drive = new MecanumDrive(hardwareMap);
+        drive = new MecanumDrive(hardwareMap pose);
         waitForStart();
     }
 }
