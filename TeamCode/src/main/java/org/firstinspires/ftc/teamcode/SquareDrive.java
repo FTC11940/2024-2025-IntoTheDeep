dc167ccad036f127da8c7f3790e1dc4257d0dfc6
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2024-10-08 14:42:39 -0400

    Update SquareDrive.java
    Semi-functional. Not crashing at least.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SquareDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SquareDrive.java
@@ -1,27 +1,104 @@
 package org.firstinspires.ftc.teamcode;
 
+import androidx.annotation.NonNull;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
 
-@Autonomous(name = "Drive in Square", group = "Autonomous")
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
+@Config
+@Autonomous(name = "BLUE_TEST_AUTO_PIXEL", group = "Autonomous")
 public class SquareDrive extends LinearOpMode {
 
-@Override
-public void runOpMode() {
-    Pose2d initialPose = new Pose2d(0, 0, Math.toRadians(0));
-    MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
 
-    TrajectoryActionBuilder actionOne = drive.actionBuilder(initialPose)
-            .lineToXSplineHeading(10.0, 0);
-    }
+    @Override
+    public void runOpMode() {
+        Pose2d initialPose = new Pose2d(0, 0, Math.toRadians(90));
+        //        Pose2d initialPose = new Pose2d(11.8, 61.7, Math.toRadians(90)); // Original
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
 
+        // vision here that outputs position
+        int visionOutputPosition = 1;
+
+        TrajectoryActionBuilder tab1 = drive.actionBuilder(initialPose)
+                /*
+                .lineToYSplineHeading(33, Math.toRadians(0))
+                .waitSeconds(2)
+                .setTangent(Math.toRadians(90))
+                .lineToY(48)
+                .setTangent(Math.toRadians(0))
+                .lineToX(32)
+                .strafeTo(new Vector2d(44.5, 30))
+                .turn(Math.toRadians(180))
+                .lineToX(47.5)
+                .waitSeconds(3);
+                 */
+                
+                .lineToY(48)
+                .waitSeconds(3);
+        TrajectoryActionBuilder tab2 = drive.actionBuilder(initialPose)
+                /*
+                .lineToY(37)
+                .setTangent(Math.toRadians(0))
+                .lineToX(18)
+                .waitSeconds(3)
+                .setTangent(Math.toRadians(0))
+                .lineToXSplineHeading(46, Math.toRadians(180))
+                 */
+
+                .waitSeconds(3);
+
+        TrajectoryActionBuilder tab3 = drive.actionBuilder(initialPose)
+                /*
+                .lineToYSplineHeading(33, Math.toRadians(180)) // 33, 180
+                .waitSeconds(2)
+                .strafeTo(new Vector2d(46, 30))
+                 */
+                .waitSeconds(3);
+        Action trajectoryActionCloseOut = tab1.fresh()
+                .strafeTo(new Vector2d(48, 12))
+                .build();
+
+
+        while (!isStopRequested() && !opModeIsActive()) {
+            int position = visionOutputPosition;
+            telemetry.addData("Position during Init", position);
+            telemetry.update();
+        }
+
+        int startPosition = visionOutputPosition;
+        telemetry.addData("Starting Position", startPosition);
+        telemetry.update();
         waitForStart();
 
-        if (isStopRequested) return
+        if (isStopRequested()) return;
 
-                drive.followTrajectory(trajectory);
+        Action trajectoryActionChosen;
+        if (startPosition == 1) {
+            trajectoryActionChosen = tab1.build();
+        } else if (startPosition == 2) {
+            trajectoryActionChosen = tab2.build();
+        } else {
+            trajectoryActionChosen = tab3.build();
+        }
+
+        Actions.runBlocking(
+                new SequentialAction(
+                        trajectoryActionChosen,
+                        trajectoryActionCloseOut
+                )
+        );
     }
 }
