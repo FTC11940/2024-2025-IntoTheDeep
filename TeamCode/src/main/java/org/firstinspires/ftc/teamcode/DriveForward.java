a389ff71296c4c455e349341c6fb482bf42a12c3
ima_dinosaur <134407666+imadinosaur1@users.noreply.github.com>
2024-10-08 15:37:30 -0400

    Create DriveForward.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveForward.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Config
+@Autonomous(name = "Drive_Forward", group = "Autonomous")
+
+public class DriveForward {
+    public void runOpMode() {
+        // instantiate your MecanumDrive at a particular pose.
+        Pose2d initialPose = new Pose2d(11.8, 61.7, Math.toRadians(90));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+
+    }
+}
